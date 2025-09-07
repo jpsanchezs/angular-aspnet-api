6ab6d3ac2c50a626cead96fe517cde074905f816
jpsanchez <pablosanchez19@estudiantec.cr>
2025-09-07 12:03:06 -0600

    Angular App y conexión

--- aspnet-api/Program.cs
+++ aspnet-api/Program.cs
@@ -2,25 +2,37 @@ using aspnet_api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// 🔐 Configurar CORS para permitir Angular
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("DevPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+// 📦 Servicios
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddControllers();
 builder.Services.AddScoped<EmpleadoRepository>();
 
 var app = builder.Build();
 
+// 🧱 Middleware
+app.UseCors("DevPolicy"); // ← Activar la política CORS
+
 app.MapControllers();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// 🌤️ Endpoint de prueba
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -28,7 +40,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
